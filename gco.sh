ddebcf5e540d44f4fb7da7a872900f953487114d
L0LZ666H05T <36075615+l0lz666h05t@users.noreply.github.com>
2018-08-06 10:43:11 +0700

    Add files via upload

---
+++ gco.sh
@@ -0,0 +1,44 @@
+clear
+toilet -f bigmono9 -F gay GCO SPAM
+sleep 1
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+echo "\033[31;1m Majalengka Cyber Team"
+echo "\033[36;1m author by :"
+echo "\033[36;1m L0LZ666H05T"
+echo "\033[32;1m Sebelum memilih spam,terlebih dahulu install bahan (08)"
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+
+date|lolcat
+echo ""
+sleep 1
+     echo "\033[34;1m SPAM SMS Yang Tersedia "
+     echo "   '(01)'JDID "
+sleep 1
+echo 
+     echo "    (02)Install Bahan"
+     echo "    (03)Keluar "
+echo " pilih nomer nya "
+read ezz
+if [ $ezz = 1 ] || [ $ezz = 01 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f mono12 -F gay "JDID"
+php jdid.php
+fi
+
+if [ $ezz = 08 ] || [ $ezz =2 ]
+then
+clear
+echo "Loading ----------------------------------"|lolcat
+sh install.sh
+sh gco.sh
+fi
+
+if [ $ezz = 09 ] || [ $ezz =3 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "KELUAR"
+echo "Good Luck ! !"
+fi
